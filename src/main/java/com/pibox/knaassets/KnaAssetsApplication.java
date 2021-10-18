2552426e796f48e1514198240fd78eb6a314e512
Valentin Piho <val.piho@gmail.com>
2021-10-18 18:29:42 +0300

    Init commit

---
+++ src/main/java/com/pibox/knaassets/KnaAssetsApplication.java
@@ -0,0 +1,13 @@
+package com.pibox.knaassets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnaAssetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnaAssetsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pibox/knaassets/KnaAssetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pibox.knaassets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnaAssetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

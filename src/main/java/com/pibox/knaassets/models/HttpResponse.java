3e700168a42be2b66e4aba7c9f37cb5d18d08a3b
Valentin Piho <val.piho@gmail.com>
2021-10-19 21:22:46 +0300

    Custom response model

---
+++ src/main/java/com/pibox/knaassets/models/HttpResponse.java
@@ -0,0 +1,25 @@
+package com.pibox.knaassets.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+public class HttpResponse {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss", timezone = "Europe/Tallinn")
+    private Date timeStamp;
+    private int httpStatusCode;
+    private HttpStatus httpStatus;
+    private String message;
+
+    public HttpResponse(int httpStatusCode, HttpStatus httpStatus, String message) {
+        this.timeStamp = new Date();
+        this.httpStatusCode = httpStatusCode;
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+}

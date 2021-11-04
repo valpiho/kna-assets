349623945bbf72b95c5c28c05e4f53d18c314965
Valentin Piho <val.piho@gmail.com>
2021-11-04 18:36:07 +0200

    Removing LOGGER. addCompany() return update

--- src/main/java/com/pibox/knaassets/api/CompanyController.java
+++ src/main/java/com/pibox/knaassets/api/CompanyController.java
@@ -5,8 +5,7 @@ import com.pibox.knaassets.api.mapper.CompanyMapper;
 import com.pibox.knaassets.company.Company;
 import com.pibox.knaassets.company.CompanyService;
 import com.pibox.knaassets.api.models.HttpResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.pibox.knaassets.exceptions.domain.ExistException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +19,6 @@ public class CompanyController {
 
     private final CompanyService companyService;
     private final CompanyMapper companyMapper;
-    private final Logger LOGGER = LoggerFactory.getLogger(getClass());
 
     public CompanyController(CompanyService companyService,
                              CompanyMapper companyMapper) {
@@ -30,10 +28,9 @@ public class CompanyController {
 
     @PostMapping
     // TODO: Available for managers
-    public ResponseEntity<HttpResponse> addCompany(@RequestBody CompanyDto companyDto) {
+    public ResponseEntity<HttpResponse> addCompany(@RequestBody CompanyDto companyDto) throws ExistException {
         Company newCompany = companyService.addCompany(companyMapper.toCompany(companyDto));
-        LOGGER.info("A new company was added successfully.");
-        return response(HttpStatus.OK, "A new company \"" + companyDto.getTitle() + "\" was added successfully.");
+        return response(HttpStatus.CREATED, "A new company \"" + newCompany.getTitle() + "\" was added successfully.");
     }
 
 
